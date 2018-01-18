c444648aff3d2fdd70d77226d662e464ef725be7
Marwan SENTENAC <m_sentenac@hotmail.com>
2018-01-18 13:28:32 +0100

    FIX ADDPIN

--- src/database/server.js
+++ src/database/server.js
@@ -6,23 +6,25 @@ let places = firebase.database().ref('places')
 export const bindDB = () => {
   places.on('value', snapshot => {
     const val = snapshot.val()
-    Object.keys(val).map(e => val[e]).map(e => {
-      updateData({
-        'type': 'Feature',
-        'properties': {
-          id: e._id,
-          name: e.name,
-          description: e.description
-        },
-        'geometry': {
-          'type': 'Point',
-          'coordinates': [
-            e.localisation.lng,
-            e.localisation.lat
-          ]
-        }
+    if (val !== null) {
+      Object.keys(val).map(e => val[e]).map(e => {
+        updateData({
+          'type': 'Feature',
+          'properties': {
+            id: e._id,
+            name: e.name,
+            description: e.description
+          },
+          'geometry': {
+            'type': 'Point',
+            'coordinates': [
+              e.localisation.lng,
+              e.localisation.lat
+            ]
+          }
+        })
       })
-    })
+    }
   })
 }
 
